6494c267335347dfde68f08ca0fce0d92126c5f3
Mohammed9531 <mohd.shoukath7866@gmail.com>
2017-06-27 00:02:37 -0500

    (refactor) Moved common code to shared folder.

---
+++ client/app/shared/components/profile/profile.model.ts
@@ -0,0 +1,12 @@
+export interface Profile {
+    imagePath: string;
+    fname: string;
+    lname: string;
+    dob: string;
+    gender: string;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zipCode: string;
+}
